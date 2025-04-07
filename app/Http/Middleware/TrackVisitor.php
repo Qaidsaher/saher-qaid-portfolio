248f31fc39b99c8193f2a85eda4a42ae00430713
SaherQaid <saherqaid2020@gmail.com>
2025-04-08 00:10:24 +0300

    new version deploy2

--- app/Http/Middleware/TrackVisitor.php
+++ app/Http/Middleware/TrackVisitor.php
@@ -18,26 +18,26 @@ class TrackVisitor
      */
     public function handle(Request $request, Closure $next)
     {
-        $sessionId = session()->getId();
-        $ip = $request->ip();
+    //     $sessionId = session()->getId();
+    //     $ip = $request->ip();
 
-        // If running locally, avoid GeoIP lookup which returns the default location.
-        if ($ip === '127.0.0.1' || $ip === '::1') {
-            $country = 'Localhost';
-        } else {
-            // Retrieve location information using the GeoIP package.
-            $location = GeoIP::getLocation($ip);
-            $country = $location['country'] ?? 'Unknown';
-        }
+    //     // If running locally, avoid GeoIP lookup which returns the default location.
+    //     if ($ip === '127.0.0.1' || $ip === '::1') {
+    //         $country = 'Localhost';
+    //     } else {
+    //         // Retrieve location information using the GeoIP package.
+    //         $location = GeoIP::getLocation($ip);
+    //         $country = $location['country'] ?? 'Unknown';
+    //     }
 
-        Visitor::updateOrCreate(
-            ['session_id' => $sessionId],
-            [
-                'last_active' => now(),
-                'ip'          => $ip,
-                'country'     => $country,
-            ]
-        );
+        // Visitor::updateOrCreate(
+        //     ['session_id' => $sessionId],
+        //     [
+        //         'last_active' => now(),
+        //         'ip'          => $ip,
+        //         'country'     => $country,
+        //     ]
+        // );
 
         return $next($request);
     }
